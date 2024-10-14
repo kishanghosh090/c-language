d9511ab73f199a63b97bac2928e7ced7cbd2984e
kishanghosh090 <kishanghosh090@gmail.com>
2024-10-15 00:05:43 +0530

    file added

--- day7/pointer.c
+++ day7/pointer.c
@@ -3,19 +3,20 @@ int main()
 {
     int age = 100;
     int *ptr = &age;
+    int **ptrr = &ptr;
     int _age = *ptr;
-    // printf("%d and %d ",&(_age), *ptr);
+    // printf("%d and %d ", &(_age), *ptr);
     // printf("%d", &i);
-    printf("%u \n", &age);
+    printf("%u  \n", &age);
     printf("%u \n", ptr);
-    printf("%u \n", *ptr); // * value at address ptr
-    printf("%u \n", &ptr); // addresss of ptr
+    // printf("%u \n", *ptr)  ; // * value at address ptr
+    // printf("%u \n", &ptr); // addresss of ptr
+    // printf("%u", ptrr);    // address of ptr (pointer to pointer )
+    // printf("break\n");
 
-    printf("break\n");
-
-    printf("%d \n", &age);
-    printf("%d \n", *ptr);
-    printf("%d \n", *(&age));
+    // printf("%d \n", &age);
+    // printf("%d \n", *ptr);
+    // printf("%d \n", *(&age));
     return 0;
 }
 
