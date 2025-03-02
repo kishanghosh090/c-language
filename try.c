27307063b3c3b58986e7391bba3c70dd1732afa5
kishanghosh090 <kishanghosh090@gmail.com>
2025-03-02 20:34:03 +0530

    added

---
+++ try.c
@@ -0,0 +1,93 @@
+// #include<stdio.h>
+// int main(){
+//     int i = 66;
+//     char j = 'B';
+//     int k = 10;
+//     if(i == j){
+//         printf("True");
+//     }else{
+//         printf("False");
+//     }
+//     printf("%c", k);
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+// //    int i = 66;
+// //    float j = (float)i;
+// //    printf("\n%f\n", j);
+// //    char name = "kishan";
+
+//    int arr[3][3] = {
+//     {1,2,3},
+//     {4,5,6},
+//     {7,8,9}
+//     };
+
+//    int num = 10;
+//    int sum = 0;
+//    for (int i = 1; i <= num; i++)
+//    {
+//     sum = sum + i;
+//    }
+//    int avg = sum / num;
+//    printf("%d", avg);
+//     return 0;
+// }
+
+// #include<stdio.h>
+
+// int main(){
+
+//     int num = 234;
+//     int rev = 0;
+//     // rev = 432
+//     // reverse the number
+//     while(num > 0 ){
+//         int rem = num % 10;
+
+//         rev = rev * 10 + rem;
+
+
+//         num = num / 10;
+//     }
+//     printf("\n%d\n", rev);
+
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
+    // &, |, ^, ~, <<, >>
+    // int a = 10;
+    // int b = 5;
+    // printf("\n%d\n", a & b);
+
+    // int const var = 189;
+    // const int var2 = 10867;
+    // var = 89;
+    // printf("%d\n", var);
+    // printf("%d", var2);
+
+    // int i = 10;
+    // printf("%d %d \n", i++, i);
+
+    // char x[] = "hello hi";
+    // printf("\n%d %d\n", sizeof(*x), sizeof(x));
+
+
+    // int a = 25;
+    // printf("    %o %x  ", a,a);
+
+    // int a = 5;
+    // printf("         %d  " , a < 1);
+
+    if(1){
+        printf("         True");
+    }
+    return 0;
+}
