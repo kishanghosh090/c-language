062d7fa93e2cae15a5add04b02bc2a8dbf6c1d9c
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-23 01:19:28 +0530

    added

--- day2/datatypes.c
+++ day2/datatypes.c
@@ -1,18 +1,55 @@
+#include <stdio.h>
+
+// data typess in c language
+
+int main(){
+    int a = 1;
+    float b = 3.2;
+    char c = 'a';
+    double d = 3.2;
+    printf("sizeof int : %d bytes\n", sizeof(a));
+    printf("sizeof char : %d bytes\n", sizeof(b));
+    printf("sizeof float : %d bytes\n", sizeof(c));
+    printf("sizeof double : %d bytes\n", sizeof(d));
+    printf("this is end of program \n");
+    // sizeof int : 4 bytes sizeof char : 1 bytes sizeof float : 4 bytes
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // data typess in c language
 // int a = 1;
 // float b = 3.2;
 // char c = 'akash';
-#include <stdio.h>
-int main()
-{
-    // volatile b = 12;
-    // b = 13;
-    // printf("%d",b);
-    // return 0;
-    printf("sizeof int : %zu bytes\n", sizeof(int));
-    printf("sizeof char : %zu bytes\n", sizeof(char));
-    printf("sizeof float : %zu bytes\n", sizeof(float));
-    printf("hi this is end of program \n");
-    // sizeof int : 4 bytes sizeof char : 1 bytes sizeof float : 4 bytes
-    return 0;
-}
+// int main()
+// {
+//     // volatile b = 12;
+//     // b = 13;
+//     // printf("%d",b);
+//     // return 0;
+//     printf("sizeof int : %zu bytes\n", sizeof(int));
+//     printf("sizeof char : %zu bytes\n", sizeof(char));
+//     printf("sizeof float : %zu bytes\n", sizeof(float));
+//     printf("hi this is end of program \n");
+//     // sizeof int : 4 bytes sizeof char : 1 bytes sizeof float : 4 bytes
+//     return 0;
+// }
